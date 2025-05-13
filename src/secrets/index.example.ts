be08cef0fc9b0d8798a1f0c28fe9e8ce1a7d9f25
official-tbabs <victorotbabs@gmail.com>
2025-05-13 11:27:49 +0100

    Stop tracking secrets/index.ts

---
+++ src/secrets/index.example.ts
@@ -0,0 +1,4 @@
+export const GOOGLE_PLACE_API_SECRET =
+  'your-google-place-api-secret-here';
+export const CLOUDINARY_CLOUD_NAME = 'your-cloudinary-cloud-name-here';
+export const CLOUDINARY_UPLOAD_PRESET = 'your-cloudinary-upload-preset-here';
